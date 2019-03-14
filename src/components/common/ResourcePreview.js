399bc15df63ef6fb60a1b713803740dea1b46bb7
Tuomas Kanerva <tuomas@kanerva.info>
2019-03-14 16:58:24 +0200

    fixed bugs

--- src/components/common/ResourcePreview.js
+++ src/components/common/ResourcePreview.js
@@ -1,9 +1,12 @@
+let FontStyleSheet = require('components/common/FontStyleSheet.js')
+
 require('./resource-preview.styl')
 
 module.exports = ({ resource, modifiers='' }) =>
     <div className={ modifiersToClass('resource-preview', modifiers) }>
         {resource.type === 'font'
             ? <span style={{ fontFamily: resource.id ? JSON.stringify(resource.id) : undefined }}>
+                <FontStyleSheet />
                 The quick brown fox jumps over the lazy dog
             </span>
             : <img src={ resource.body } alt='thumbnail' />}
--- src/components/views/designer/elementShapes.js
+++ src/components/views/designer/elementShapes.js
@@ -1,43 +1,45 @@
 module.exports = r.mapObjIndexed(
     (fn) => (it, className='element-view__canvas', isBack) => {
-        let component = fn(it)
+        let component = fn(it, isBack)
 
         return React.cloneElement(component, {
             ...component.props,
-            className,
-            ...(isBack ? { x: -it.width - 20 } : {})
+            className
         })
     },
 
     {
-        rect: ({ width, height }) =>
+        rect: ({ width, height }, isBack) =>
             <rect
                 width={ width }
                 height={ height }
-                x='0'
+                x={ isBack ? -width - 20 : 0 }
                 y='0' />,
 
-        roundedRect: ({ width, height }) =>
+        roundedRect: ({ width, height }, isBack) =>
             <rect
                 width={ width }
                 height={ height }
                 rx='5'
                 ry='5'
-                x='0'
+                x={ isBack ? -width - 20 : 0 }
                 y='0' />,
 
-        ellipse: ({ width, height }) =>
+        ellipse: ({ width, height }, isBack) =>
             <ellipse
                 rx={ width / 2 }
                 ry={ height / 2 }
-                cx={ width / 2 }
+                cx={ isBack ? -width / 2 - 20 : width / 2 }
                 cy={ height / 2}
                 x='0'
                 y='0' />,
 
-        hexagon: ({ width, height }) =>
-            <polygon
-                points={ `0,${height / 2} ${width / 4},0 ${width / 4 * 3},0 ${width},${height / 2}, ${width / 4 * 3},${height} ${width / 4},${height} 0,${height / 2}` }
-                />,
+        hexagon: ({ width, height }, isBack) => {
+            let zero = isBack ? -width - 20 : 0
+
+            return <polygon
+                points={ `${zero},${height / 2} ${zero + width / 4},0 ${zero + width / 4 * 3},0 ${zero + width},${height / 2} ${zero + width / 4 * 3},${height} ${zero + width / 4},${height} ${zero},${height / 2}` }
+                />
+        }
     }
 )
